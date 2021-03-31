c0ed720286e611a9f8ca892ee87b6c70c6d1b95f
shubh114sh <65242431+shubh114sh@users.noreply.github.com>
2021-03-31 08:40:25 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,1 +1,15 @@
 
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+	{
+		username: String,
+		email: String,
+		password: String
+	},
+	{ collection: 'users' }
+)
+
+const model = mongoose.model('UserSchema', UserSchema)
+
+module.exports = model
